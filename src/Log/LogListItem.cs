0ed8d75b049e8e2e8ec1715ebeeb271668f4fe21
FrKaram <francois.karam@kaliconseil.Fr>
2018-05-03 11:57:54 +0200

    Fixed Ignored File Issue

---
+++ src/Log/LogListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KS2Drive.Log
+{
+    public class LogListItem
+    {
+        public String Date { get; set; }
+        public String Object { get; set; }
+        public String Action { get; set; }
+        public String Fichier { get; set; }
+        public String Resultat { get; set; }
+    }
+}
